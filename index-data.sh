6a4882be1bf132cff5c98d82a60c9ff19e9ee901
Mike Lambert <lambert8@illinois.edu>
2017-04-19 21:50:48 +0000

    Fix path

--- index-data.sh
+++ index-data.sh
@@ -10,5 +10,5 @@ curl -XPUT -u elastic:$passwd "localhost:8000/${index_name}?pretty&pretty"
 # Add all documents from /data/bioCaddie to the index
 for i in $(seq 1 794992); do
         echo "Indexing $i to localhost:8000/${index_name}/${index_type}/${i}?pretty&pretty"
-        curl -XPUT -u elastic:$passwd "localhost:8000/${index_name}/${index_type}/${i}?pretty&pretty" -H 'Content-Type: application/json' -d "@/data/bioCaddie/update_json_folder/${i}.json"
+        curl -XPUT -u elastic:$passwd "localhost:8000/${index_name}/${index_type}/${i}?pretty&pretty" -H 'Content-Type: application/json' -d "@/data/bioCaddie/data/update_json_folder/${i}.json"
 done
